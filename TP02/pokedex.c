38caebe866f7dbfd805dc0c5b081b442de63de80
Paulo Henrique <paulohenriqueldp@gmail.com>
2024-09-27 11:51:18 -0300

    TP02

---
+++ TP02/pokedex.c
@@ -0,0 +1,188 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+char ids[801][5];
+int numIds = 0;
+
+typedef struct {
+    char id[8];
+    char generation[5];
+    char name[50];
+    char description[100];
+    char types[2][30];
+    char abilities[1000];
+    char weight[10];
+    char height[10];
+    char captureRate[10];
+    bool isLegendary;
+    char captureDate[15];
+} Pokemon;
+
+char * replace(
+    char const * const original, 
+    char const * const pattern, 
+    char const * const replacement
+) {
+  size_t const replen = strlen(replacement);
+  size_t const patlen = strlen(pattern);
+  size_t const orilen = strlen(original);
+
+  size_t patcnt = 0;
+  const char * oriptr;
+  const char * patloc;
+
+  for (oriptr = original; patloc = strstr(oriptr, pattern); oriptr = patloc + patlen) {
+    patcnt++;
+  }
+
+  {
+    size_t const retlen = orilen + patcnt * (replen - patlen);
+    char * const returned = (char *) malloc( sizeof(char) * (retlen + 1) );
+
+    if (returned != NULL)
+    {
+      char * retptr = returned;
+      for (oriptr = original; patloc = strstr(oriptr, pattern); oriptr = patloc + patlen)
+      {
+        size_t const skplen = patloc - oriptr;
+        strncpy(retptr, oriptr, skplen);
+        retptr += skplen;
+        strncpy(retptr, replacement, replen);
+        retptr += replen;
+      }
+      strcpy(retptr, oriptr);
+    }
+    return returned;
+  }
+}
+
+void imprimir(Pokemon pokemon) {
+    printf("[#%s -> %s: %s - ", pokemon.id, pokemon.name, pokemon.description);
+    if(strcmp(pokemon.types[1], " ") == 0) {
+        printf("%s - ", pokemon.types[0]);
+    } else {
+        printf("%s, %s - ", pokemon.types[0], pokemon.types[1]);
+    }
+    printf("%s", pokemon.abilities);
+    printf(" - ");
+    printf("%skg - %sm - %s%% - %s - %s] %s\n", pokemon.weight,
+           pokemon.height, pokemon.captureRate,
+           pokemon.isLegendary ? "true" : "false",
+           pokemon.generation, pokemon.captureDate);
+}
+
+void ler() {
+    while (numIds < 801) {
+        scanf("%s", ids[numIds]);
+        if (strcmp(ids[numIds], "FIM") == 0) {
+            return;
+        }
+        numIds++;
+    }
+}
+
+Pokemon parsePokemon(char *linha) {
+    Pokemon pokemon;
+    int contador = 1;
+    char type1[30];
+    char type2[30] = "";
+    char *token = strtok(linha, ",");
+
+    while (token) {
+        switch (contador) {
+            case 1:
+                strcpy(pokemon.id, token);
+                break;
+            case 2:
+                strcpy(pokemon.generation, token);
+                break;
+            case 3:
+                strcpy(pokemon.name, token);
+                break;
+            case 4:
+                strcpy(pokemon.description, token);
+                break;
+            case 5:
+                strcpy(type1, token);
+                break;
+            case 6:
+                strcpy(type2, token);
+                break;
+            case 7: {
+                char *formataHabilidades;
+                formataHabilidades = replace(token, ";", ", ");
+                strcpy(pokemon.abilities, formataHabilidades);
+                break;
+            }
+            case 8:
+                strcpy(pokemon.weight, token);
+                break;
+            case 9:
+                strcpy(pokemon.height, token);
+                break;
+            case 10:
+                strcpy(pokemon.captureRate, token);
+                break;
+            case 11:
+                pokemon.isLegendary = strcmp(token, "0") != 0;
+                break;
+            case 12:
+                strcpy(pokemon.captureDate, token);
+                break;
+            default:
+                break;
+        }
+        token = strtok(NULL, ",");
+        contador++;
+    }
+
+    strcpy(pokemon.types[0], type1);
+    if (strlen(type2) > 0) {
+        strcpy(pokemon.types[1], type2);
+    } else {
+        strcpy(pokemon.types[1], "");
+    }
+
+    return pokemon;
+}
+
+
+int main() {
+    FILE *file = fopen("/home/paulo/Documentos/Programação/Exercicios-Aeds2/TP02/pokemon.csv", "r");
+
+    if (!file) {
+        printf("Não foi possível abrir o arquivo.");
+        return 1;
+    }
+
+    char linha[400];
+    Pokemon listaPokemon[802];
+
+    ler();
+
+    while (fgets(linha, sizeof(linha), file) != NULL) {
+        linha[strcspn(linha, "\n")] = '\0';
+        char compareString[400];
+        char *auxstring;
+        auxstring = replace(linha, ", ", ";");
+        auxstring = replace(auxstring, ",,", ", ,");
+        auxstring = replace(auxstring, "\"", "");
+
+        strcpy(compareString, linha);
+        char *idToken = strtok(compareString, ",");
+        for (int i = 0; i < numIds; i++) {
+            if (strcmp(ids[i], idToken) == 0) {
+                listaPokemon[i] = parsePokemon(auxstring);
+            }
+        }
+    }
+
+    fclose(file);
+
+    for (int j = 0; j < numIds; j++) {
+        imprimir(listaPokemon[j]);
+    }
+
+    return 0;
+}
